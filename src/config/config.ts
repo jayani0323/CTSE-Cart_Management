78b9006ba6a9cb780628ad4886833713b310b41c
jayani98 <Jayani Saumya>
2022-05-17 01:45:56 +0530

    Update workflow

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,10 +4,7 @@ import * as dotenv from "dotenv"
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
-if (envFound.error) {
-    // This error should crash whole process
-    throw new Error("Couldn't find .env file");
-}
+
 
 export default {
     port: parseInt(process.env.PORT||"3005", 10),
